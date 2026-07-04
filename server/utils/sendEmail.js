a6c8d5e3b493e9862a901de5bc3e8b9e54e20b7b
santhosh-web-code <santhoshnanisanka@gmail.com>
2026-07-04 21:57:26 +0530

    Update Nodemailer Gmail transporter and add SMTP verification

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,17 +3,18 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false
-      }
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
     });
+
+    await transporter.verify();
+    console.log("SMTP Connected");
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
