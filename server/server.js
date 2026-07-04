dd91a14811f1ab0679f7a392a0c554cf31e09d67
santhosh-web-code <santhoshnanisanka@gmail.com>
2026-07-04 09:32:09 +0530

    Fix CORS for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,26 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://hireloop-frontend.vercel.app',
+  'https://hireloop-frontend-git-main-santhosh-kumar-sankas-projects.vercel.app',
+  'https://hireloop-frontend-po5baa57z-santhosh-kumar-sankas-projects.vercel.app'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
